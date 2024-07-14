271c1ac20012b2a9c5eee74e43fdb6b7dcab5cab
Samarendra Gouda <samarendragouda@gmail.com>
2024-07-14 05:27:09 +0200

    Revert "feat: add-modals"
    This reverts commit 18f5d542946a4ba88aaec149f9c9d4f6b376dcea.

--- market-nuxt/app.vue
+++ market-nuxt/app.vue
@@ -2,34 +2,6 @@
   <div>
     <NuxtLayout>
       <NuxtPage />
-      <Modals />
     </NuxtLayout>
   </div>
 </template>
-
-<style>
-.modals-enter .modal-inner {
-  @apply ease-out duration-300 sm:duration-200;
-}
-
-.modals-enter-active .modal-inner,
-.modals-leave-active .modal-inner,
-.modals-leave .modal-inner {
-  @apply ease-in duration-300 sm:duration-200;
-}
-
-.modals.leave-from .modal-inner,
-.modals-enter-to .modal-inner {
-  @apply opacity-100 translate-y-0 sm:scale-100;
-}
-
-.modals-enter-from .modal-inner,
-.modals-leave-to .modal-inner {
-  @apply opacity-0 translate-y-96 sm:translate-y-0 sm:scale-95;
-}
-
-.modals-enter-from .modal-inner[data-sheet-position="top"],
-.modals-leave-to .modal-inner[data-sheet-position="top"] {
-  @apply opacity-0 -translate-y-32 sm:translate-y-0 sm:scale-95;
-}
-</style>
--- node_modules/.pnpm/@wagmi+vue@0.0.26_@tanstack+query-core@5.51.1_@tanstack+vue-query@5.51.1_vue@3.4.31__bufferut_uhx37aiwfsx5mmukez5oybmlpu/node_modules/@wagmi/vue/dist/types/version.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const version = "0.0.26";
-//# sourceMappingURL=version.d.ts.map
